497197927c0ab9815eb4073272c07a7940d3fdda
Ed <Ed@MacBook-Pro.local>
2020-04-05 15:12:45 +0900

    Initial commit

---
+++ ecr/config.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-sample-ed" # ここの名前は各自で考えましょう
+    key = "terraform-sample/ecr/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+provider "aws" {
+  region = "ap-northeast-1"
+}
